6397b9eda3025048e5ba795a0e0f6c2c16edacf9
Sobhan <s.kevishahi@gmail.com>
2022-11-16 20:56:16 -0500

    Implemented Basic Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float maxHP;
+    [SerializeField] float currentHP;
+    [SerializeField] float flirtPoints;
+    [SerializeField] string playerName;
+
+    public float GetMaxHP()
+    {
+        return maxHP;
+    }
+
+    public float GetCurrentHP()
+    {
+        return currentHP;
+    }
+
+    public string GetPlayerName()
+    {
+        return playerName;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHP -= damage;
+    }
+
+    public bool IsDead()
+    {
+        return currentHP <= 0;
+    }
+
+    public float GetPoints()
+    {
+        return flirtPoints;
+    }
+
 
 }
