2da64c798aec77fa8febebdb89dfdf169ce7b73c
Sobhan <s.kevishahi@gmail.com>
2022-11-16 20:56:39 -0500

    Implemented State Machine

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,12 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum STATE { START, PLAYER_TURN, ENEMY_TURN, WIN, LOSE}
 
 public class StateMachine : MonoBehaviour
 {
     private STATE currentState;
+    private ViewManager viewManager;
+    private Enemy enemy;
+    private Player player;
+
+    private void Awake()
+    {
+        viewManager = FindObjectOfType<ViewManager>();
+        enemy = FindObjectOfType<Enemy>();
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Start()
+    {
+        currentState = STATE.START;
+        Transition();
+    }
+
 
 
     private IEnumerator Transition(float pause=0f)
@@ -49,16 +67,41 @@ public class StateMachine : MonoBehaviour
 
     private void EnemyState()
     {
-        throw new NotImplementedException();
+        viewManager.SetMessage("The Dragon Isn't In The Mood To Flirt. It Breathes Fire!");
+        player.TakeDamage(enemy.GetDamage());
+        viewManager.UpdatePlayerSlider();
+
+        currentState = STATE.PLAYER_TURN;
+        Transition(2);
     }
 
     private void StartState()
     {
-        throw new NotImplementedException();
+        viewManager.SetMessage("An Attractive Dragon Approaches!");
+        currentState = STATE.PLAYER_TURN;
+        Transition(2);
     }
 
     private void PlayerState()
     {
-        
+        viewManager.SetMessage("Choose An Action.");
+    }
+
+    public void OnPlayerFlirt()
+    {
+        if (currentState != STATE.PLAYER_TURN)
+            return;
+
+        viewManager.SetMessage("You Successfully Flirt With The Dragon!");
+        enemy.TakePoints(player.GetPoints());
+        viewManager.UpdateEnemySlider();
+
+        currentState = STATE.ENEMY_TURN;
+        Transition(2);
+    }
+
+    public void OnPlayerEnd()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
