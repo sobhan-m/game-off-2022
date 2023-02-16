9dcb346d0dd5f1f12a9b93a577966cdc20927bd1
Sobhan <s.kevishahi@gmail.com>
2023-02-15 19:58:08 -0800

    Added Health Tracker Back To Game

---
+++ Assets/Scripts/HealthTracker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthTracker : MonoBehaviour
+{
+    [SerializeField] Slider healthSlider;
+
+    private Health playerHealth;
+
+    private void Start()
+    {
+        playerHealth = FindObjectOfType<Player>().playerHealth;
+
+        healthSlider.minValue = 0;
+        healthSlider.maxValue = playerHealth.maxHealth;
+        healthSlider.value = playerHealth.currentHealth;
+    }
+
+    private void Update()
+    {
+        healthSlider.value = playerHealth.currentHealth;
+    }
+}
