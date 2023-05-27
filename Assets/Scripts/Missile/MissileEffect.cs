7a2e3a9f418db894532f2cbd59dfff89ecb85b8e
Sobhan <s.kevishahi@gmail.com>
2023-05-26 21:09:20 -0700

    Added basics of `Effect`.

--- Assets/Scripts/Missile/MissileEffect.cs
+++ Assets/Scripts/Missile/MissileEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IAffectable
+public abstract class MissileEffect
 {
-	public void StoreEffect(MissileEffect missileEffect);
+
 }
