e52f183e90f7f28f69145b17e76b1c046eda2d9c
Sobhan <s.kevishahi@gmail.com>
2023-05-29 21:16:30 -0700

    Added factory method.

--- Assets/Scripts/Missile/MissileDamage.cs
+++ Assets/Scripts/Missile/MissileDamage.cs
@@ -8,4 +8,21 @@ abstract public class MissileDamage
 	public float damageAmount { get; protected set; }
 
 	abstract public void ApplyDamage(IDamageable damagedObject);
+
+	public static MissileDamage ConstructMissileDamage(PlayerMissileType damageType, float damageAmount)
+	{
+		switch (damageType)
+		{
+			case PlayerMissileType.REGULAR:
+				return new RegularDamage(damageAmount);
+			case PlayerMissileType.FIRE:
+				return new FireDamage(damageAmount);
+			case PlayerMissileType.PSYCHIC:
+				return new PsychicDamage(damageAmount);
+			case PlayerMissileType.COLD:
+				return new ColdDamage(damageAmount);
+			default:
+				throw new System.ArgumentException("Must have a valid damage type.");
+		}
+	}
 }
