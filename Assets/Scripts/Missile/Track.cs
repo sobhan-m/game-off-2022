167498cd71d982f86f15528fdfef9bfcab38edc7
Sobhan <s.kevishahi@gmail.com>
2023-06-05 21:02:37 -0700

    Added `LeftMost()` and `RightMost()` checks.

--- Assets/Scripts/Missile/Track.cs
+++ Assets/Scripts/Missile/Track.cs
@@ -4,30 +4,40 @@ using UnityEngine;
 
 public class Track
 {
-    private List<Transform> positions;
-    public int currentIndex { get; private set; }
+	private List<Transform> positions;
+	public int currentIndex { get; private set; }
 
-    public Track(List<Transform> positions, int startingIndex = 0)
-    {
-        this.positions = positions;
-        currentIndex = startingIndex;
-    }
+	public Track(List<Transform> positions, int startingIndex = 0)
+	{
+		this.positions = positions;
+		currentIndex = startingIndex;
+	}
 
-    public Transform MovePrevious()
-    {
-        currentIndex = Mathf.Clamp(currentIndex - 1, 0, positions.Count - 1);
-        return positions[currentIndex];
-    }
+	public Transform MovePrevious()
+	{
+		currentIndex = Mathf.Clamp(currentIndex - 1, 0, positions.Count - 1);
+		return positions[currentIndex];
+	}
 
-    public Transform MoveNext()
-    {
-        currentIndex = Mathf.Clamp(currentIndex + 1, 0, positions.Count - 1);
-        return positions[currentIndex];
-    }
+	public Transform MoveNext()
+	{
+		currentIndex = Mathf.Clamp(currentIndex + 1, 0, positions.Count - 1);
+		return positions[currentIndex];
+	}
 
-    public Transform CurrentPosition()
-    {
-        return positions[currentIndex];
-    }
+	public Transform CurrentPosition()
+	{
+		return positions[currentIndex];
+	}
+
+	public bool IsLeftMost()
+	{
+		return currentIndex == 0;
+	}
+
+	public bool IsRightMost()
+	{
+		return currentIndex == positions.Count - 1;
+	}
 
 }
