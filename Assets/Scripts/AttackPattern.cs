f4831dc44b128bd15cc3834f45b0e46179640703
Sobhan <s.kevishahi@gmail.com>
2023-06-06 20:53:12 -0700

    Added speed to attack pattern.

--- Assets/Scripts/AttackPattern.cs
+++ Assets/Scripts/AttackPattern.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class AttackPattern : ScriptableObject
 {
 	public GameObject missilePrefab;
 	public int numberOfAttacks;
 	public float secondsBetweenAttacks;
+	public float speed;
 }
