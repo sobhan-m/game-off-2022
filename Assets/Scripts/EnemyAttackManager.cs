3a4ae2a625cf2fc7970c613015c7e9fdaf593e21
Sobhan <s.kevishahi@gmail.com>
2023-06-06 21:03:55 -0700

    Implemented Enemy Attack Manager.

---
+++ Assets/Scripts/EnemyAttackManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackManager : MonoBehaviour
+{
+	[SerializeField] AttackPattern[] attacks;
+	[SerializeField] float secondsBetweenPatterns;
+	public Meter patternWait { get; private set; }
+	public Meter attackWait { get; private set; }
+	public Meter patternProgress { get; private set; }
+	public AttackPattern currentAttack { get; private set; }
+
+	private void Awake()
+	{
+		patternWait = new Meter(0, secondsBetweenPatterns);
+
+		ResetAttackPattern();
+	}
+
+	private void Attack()
+	{
+		GameObject missile = Instantiate(currentAttack.missilePrefab, transform.position, Quaternion.identity);
+		if (missile.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+		{
+			rb.velocity = new Vector2(0, -currentAttack.speed);
+		}
+	}
+
+	private void Update()
+	{
+		if (patternProgress.IsFull()) // We're done with this attack pattern.
+		{
+			patternWait.FillMeter(Time.deltaTime);
+		}
+		else
+		{
+			attackWait.FillMeter(Time.deltaTime);
+		}
+
+		if (patternWait.IsFull()) // We're done waiting for the next attack pattern.
+		{
+			patternProgress.EmptyMeter();
+			ResetAttackPattern();
+			attackWait.FillMeter();
+			patternWait.EmptyMeter();
+		}
+
+		if (attackWait.IsFull()) // We're done waiting for the next attack.
+		{
+			Attack();
+			patternProgress.FillMeter(1f);
+			attackWait.EmptyMeter();
+		}
+	}
+
+	private void ResetAttackPattern()
+	{
+		currentAttack = attacks[Random.Range(0, attacks.Length)];
+		attackWait = new Meter(0, currentAttack.secondsBetweenAttacks);
+		patternProgress = new Meter(0, currentAttack.numberOfAttacks);
+	}
+}
