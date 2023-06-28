237f0b2bbc6af264cfc638c9c72e00aa2b1b5a47
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-06-27 22:05:50 -0700

    Added mouse over sound to buttons.

--- Assets/Scripts/Settings/UX/ButtonSoundManager.cs
+++ Assets/Scripts/Settings/UX/ButtonSoundManager.cs
@@ -3,30 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System;
 
 public class ButtonSoundManager : MonoBehaviour
 {
-    [Tooltip("This field is not currently in use.")]
-    [SerializeField] AudioClip mouseOverSound;
-    [SerializeField] AudioClip clickSound;
+	[Tooltip("This field is not currently in use.")]
+	[SerializeField] AudioClip mouseOverSound;
+	[SerializeField] AudioClip clickSound;
 
-    private List<Button> buttons;
-    private Camera mainCamera;
+	private List<Button> buttons;
+	private Camera mainCamera;
 
-    private void Start()
-    {
-        mainCamera = Camera.main;
+	private void Start()
+	{
+		mainCamera = Camera.main;
 
-        buttons = new List<Button>(Resources.FindObjectsOfTypeAll(typeof(Button)) as Button[]);
+		buttons = new List<Button>(Resources.FindObjectsOfTypeAll(typeof(Button)) as Button[]);
 
-        foreach (Button button in buttons)
-        {
-            button.onClick.AddListener(PlayClickSound);
-        }
-    }
+		foreach (Button button in buttons)
+		{
+			button.onClick.AddListener(PlayClickSound);
 
-    public void PlayClickSound()
-    {
-        AudioSource.PlayClipAtPoint(clickSound, mainCamera.transform.position);
-    }
+			// Adding mouse over event.
+			EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
+			EventTrigger.Entry newEvent = new EventTrigger.Entry();
+			newEvent.eventID = UnityEngine.EventSystems.EventTriggerType.PointerEnter;
+			newEvent.callback.AddListener(PlayMouseOverSound);
+			trigger.triggers.Add(newEvent);
+		}
+	}
+
+
+	public void PlayClickSound()
+	{
+		AudioSource.PlayClipAtPoint(clickSound, mainCamera.transform.position);
+	}
+
+	public void PlayMouseOverSound(BaseEventData arg)
+	{
+		AudioSource.PlayClipAtPoint(mouseOverSound, mainCamera.transform.position);
+	}
 }
