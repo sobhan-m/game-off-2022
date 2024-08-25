3d24801d858ddd443c5c24219234062dfa7ee950
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-08-25 01:21:04 -0700

    fix: change credits to scroll consistently regardless of canvas size

--- Assets/Scripts/CreditMover.cs
+++ Assets/Scripts/CreditMover.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[RequireComponent(typeof(RectTransform))]
 public class CreditMover : MonoBehaviour
 {
-    [SerializeField][Min(0)] private float secondsToScroll;
-    [SerializeField] private float scrollSpeed;
-    [SerializeField] private float secondsToNextScene;
+    [SerializeField] private RectTransform endGoal;
+    [SerializeField][Min(0)] private float waitSecondsBeforeScrolling;
+    [SerializeField] private float secondsToScroll;
+    [SerializeField] private float waitSecondsBeforeLoadingNextScene;
     private bool isScrolling = false;
+    private RectTransform currentTransform;
+    private float speed;
+
+    private void Awake() {
+        currentTransform = GetComponent<RectTransform>();
+        speed = (endGoal.localPosition.y - currentTransform.localPosition.y) / secondsToScroll; // velocity = displacement / change in time
+    }
 
     private void Start()
     {
-        Invoke("StartScrolling", secondsToScroll);
-        Invoke("GoToNextScene", secondsToNextScene);
+        Invoke("StartScrolling", waitSecondsBeforeScrolling);
     }
 
     private void Update()
@@ -22,6 +29,15 @@ public class CreditMover : MonoBehaviour
         {
             Scroll();
         }
+        if (IsAtGoal() && isScrolling)
+        {
+            Invoke("GoToNextScene", waitSecondsBeforeLoadingNextScene);
+        }
+    }
+
+    private bool IsAtGoal()
+    {
+        return currentTransform.localPosition.y >= endGoal.localPosition.y;
     }
 
     private void StartScrolling()
@@ -31,8 +47,8 @@ public class CreditMover : MonoBehaviour
 
     private void Scroll()
     {
-        Vector3 position = transform.position;
-        gameObject.transform.position = new Vector3(position.x, position.y + scrollSpeed * Time.deltaTime, position.z);
+        Vector3 position = currentTransform.localPosition;
+        currentTransform.localPosition = new Vector3(position.x, position.y + speed * Time.deltaTime, position.z);
     }
 
     private void GoToNextScene()
